9e745b02bb5256e7928033525eeef3a1ab285868
h <h@59.11.98.63>
2018-12-31 06:24:18 +0900

    chapter7

---
+++ 201347028/src/chapter7/Ex07_02.java
@@ -0,0 +1,19 @@
+package chapter7;
+
+public class Ex07_02 {
+
+	public static void main(String[] args) {
+		int hap=0;
+		int i;
+		
+		i=0;
+		while(i<=10) {
+			hap=hap+1;
+			i++;
+		}
+		
+		System.out.printf(" 1에서 10까지의 합: %d \n", hap);
+
+	}
+
+}
