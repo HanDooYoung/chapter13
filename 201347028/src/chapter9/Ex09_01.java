e4f0f37971392909f027c7d7a860d298e4fc4522
h <h@192.168.219.105>
2019-01-04 13:03:11 +0900

    chapter9

---
+++ 201347028/src/chapter9/Ex09_01.java
@@ -0,0 +1,16 @@
+package chapter9;
+
+public class Ex09_01 {
+
+	public static void main(String[] args) {
+		String str="IT CookBook.java";
+		int len;
+		
+		len=str.length();
+		
+		System.out.printf("문자를 : %s \n", str);
+		System.out.printf("문자열 길이 : %d", len);
+
+	}
+
+}
---
+++ 201347028/src/chapter9/Ex09_07.java
@@ -0,0 +1,19 @@
+package chapter9;
+
+public class Ex09_07 {
+
+	public static void main(String[] args) {
+		String str=" 한글 ABCD efgh ";
+		String result="";
+		
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)!=' ')
+				result+=str.charAt(i);
+		}
+		
+		System.out.println("원 문자열 ==> ["+str+"]");
+		System.out.println("공백제거  ==>["+result+"]");
+
+	}
+
+}
