081fa68a299aff95395fc84746ebbd030eaafe84
h <h@192.168.219.104>
2019-01-07 21:59:25 +0900

    chapter11

---
+++ 201347028/src/chapter11/Ex11_11.java
@@ -0,0 +1,25 @@
+package chapter11;
+class Car11{
+	String color;
+	int speed;
+	static private int count=0;
+	
+	Car11(){
+		count++;
+	}
+	
+	static int getCount() {
+		return count;
+	}
+}
+public class Ex11_11 {
+
+	public static void main(String[] args) {
+		System.out.println("현재 생산된 자동차 숫자 ==>"+Car11.getCount());
+		
+		Car11 myCar1=new Car11();
+		System.out.println("현재 생산된 자동차 숫자 ==>"+myCar1.getCount());
+
+	}
+
+}
---
+++ 201347028/src/chapter5/Ex11.java
@@ -0,0 +1,48 @@
+package chapter5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex11 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		int a,b;
+		char c;
+		bw.write("첫번째 수를 입력하세요 : ");
+		bw.flush();
+		a=Integer.parseInt(br.readLine());
+		bw.write("계산할 연산자를 입력하세요 : ");
+		bw.flush();
+		c=br.readLine().charAt(0);
+		bw.write("두번째 수를 입력하세요 : ");
+		bw.flush();
+		b=Integer.parseInt(br.readLine());
+		if(c=='+')	{
+			bw.write(a+" "+c+" "+b+" = "+(a+b));	
+			bw.close();
+		}
+		if(c=='-')	{
+			bw.write(a+" "+c+" "+b+" = "+(a-b));
+			bw.close();
+		}
+		if(c=='*')	{
+			bw.write(a+" "+c+" "+b+" = "+(a*b));
+			bw.close();
+		}
+		if(c=='/')	{
+			bw.write(a+" "+c+" "+b+" = "+((double)a/(double)b));
+			bw.close();
+		}
+		if(c=='%')	{
+			bw.write(a+" "+c+" "+b+" = "+(a%b));
+			bw.close();
+		}
+
+	}
+
+}
